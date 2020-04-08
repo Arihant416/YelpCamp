3d1bba34b4a194788f8bbe649d35c11da4ba64d9
Arihant416 <arihantjain416@gmail.com>
2020-04-08 15:24:14 +0530

    Profiler

--- ver12/models/user.js
+++ ver12/models/user.js
@@ -1,11 +1,15 @@
-var mongoose = require('mongoose')
-var passportLocalMongoose = require('passport-local-mongoose')
+var mongoose = require ('mongoose');
+var passportLocalMongoose = require ('passport-local-mongoose');
 
-var UserSchema = new mongoose.Schema({
+var UserSchema = new mongoose.Schema ({
   username: String,
   password: String,
-  isAdmin: { type: Boolean, default: false }
-})
+  avatar: String,
+  firstName: String,
+  lastName: String,
+  email: String,
+  isAdmin: {type: Boolean, default: false},
+});
 
-UserSchema.plugin(passportLocalMongoose)
-module.exports = mongoose.model('User', UserSchema)
+UserSchema.plugin (passportLocalMongoose);
+module.exports = mongoose.model ('User', UserSchema);
