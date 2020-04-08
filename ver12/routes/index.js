1b74795a0ecb14b77c0f2691c9dba8282df1a03d
Arihant416 <arihantjain416@gmail.com>
2020-04-08 15:24:47 +0530

    Routing to Clicked Profile

--- ver12/routes/index.js
+++ ver12/routes/index.js
@@ -1,58 +1,85 @@
-const express = require('express'),
-  router = express.Router(),
-  passport = require('passport'),
-  User = require('../models/user')
+const express = require ('express'),
+  router = express.Router (),
+  passport = require ('passport'),
+  User = require ('../models/user'),
+  Campground = require ('../models/campground');
 
 //Root Route
-router.get('/', function (req, res) {
-  res.render('landing')
-})
+router.get ('/', function (req, res) {
+  res.render ('landing');
+});
 
 // Auth Routes
 
 // show register form
-router.get('/register', function (req, res) {
-  res.render('register', { page: 'register' })
-})
+router.get ('/register', function (req, res) {
+  res.render ('register', {page: 'register'});
+});
 
 //show login form
-router.get('/login', function (req, res) {
-  res.render('login', { page: 'login' })
-})
+router.get ('/login', function (req, res) {
+  res.render ('login', {page: 'login'});
+});
 //Handling Sign Up Logic
 
-router.post('/register', function (req, res) {
-  var newUser = new User({ username: req.body.username })
+router.post ('/register', function (req, res) {
+  var newUser = new User ({
+    username: req.body.username,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    avatar: req.body.avatar,
+  });
   if (req.body.adminCode === 'hashcode1') {
-    newUser.isAdmin = true
+    newUser.isAdmin = true;
   }
-  User.register(newUser, req.body.password, function (err, user) {
+  User.register (newUser, req.body.password, function (err, user) {
     if (err) {
-      console.log(err)
-      return res.render('register', { error: err.message })
+      console.log (err);
+      return res.render ('register', {error: err.message});
     }
-    passport.authenticate('local')(req, res, function () {
-      req.flash('success', 'Welcome To YelpCamp, ' + user.username)
-      res.redirect('/campgrounds')
-    })
-  })
-})
+    passport.authenticate ('local') (req, res, function () {
+      req.flash ('success', 'Welcome To YelpCamp, ' + user.username);
+      res.redirect ('/campgrounds');
+    });
+  });
+});
 
 //handling login logic
-router.post(
+router.post (
   '/login',
-  passport.authenticate('local', {
+  passport.authenticate ('local', {
     successRedirect: '/campgrounds',
-    failureRedirect: '/login'
+    failureRedirect: '/login',
   }),
   function (req, res) {}
-)
+);
 
 //Logout Route
-router.get('/logout', function (req, res) {
-  req.logout()
-  req.flash('success', 'Logged You Out Successfully !')
-  res.redirect('/campgrounds')
-})
+router.get ('/logout', function (req, res) {
+  req.logout ();
+  req.flash ('success', 'Logged You Out Successfully !');
+  res.redirect ('/campgrounds');
+});
 
-module.exports = router
+//USER's profile
+router.get ('/users/:id', function (req, res) {
+  User.findById (req.params.id, function (err, foundUser) {
+    if (err) {
+      req.flash ('error', 'Something went Wrong');
+      res.redirect ('/');
+    }
+    Campground.find ()
+      .where ('author.id')
+      .equals (foundUser._id)
+      .exec (function (err, campgrounds) {
+        if (err) {
+          req.flash ('error', 'Something went Wrong');
+          res.redirect ('/');
+        }
+        res.render ('users/show', {user: foundUser, campgrounds: campgrounds});
+      });
+  });
+});
+
+module.exports = router;
